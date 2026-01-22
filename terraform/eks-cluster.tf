b4ba2d4067f6663f6609edcf29800e68e20bb906
mix130913865-W <wtfbebez@gmail.com>
2026-01-22 18:09:39 +0800

    Add comments for EKS cluster configuration

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,38 +1,41 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  source  = "terraform-aws-modules/eks/aws"  # 使用官方 EKS Terraform module
+  version = "19.19.1"                        # 指定 module 版本，確保環境一致
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.29"
+  # Cluster 基本設定
+  cluster_name    = local.cluster_name       # EKS Cluster name，從 local 變數取得
+  cluster_version = "1.29"                   # Kubernetes version
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  # VPC 與 Subnet 設定
+  vpc_id     = module.vpc.vpc_id            # 指定已建立的 VPC ID
+  subnet_ids = module.vpc.private_subnets   # 使用 VPC 的 private Subnets
+  cluster_endpoint_public_access = true     # Cluster API endpoint 是否公開可存取
 
+  # EKS Managed Node Group 預設設定
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
+    ami_type = "AL2_x86_64"                 # EC2 AMI type，Amazon Linux 2 x86_64
   }
 
+  # EKS Managed Node Groups 詳細設定
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "node-group-1"                 # Node Group name
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.small"]         # EC2 instance types
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size     = 1                       # Auto Scaling 最小 Node 數量
+      max_size     = 3                       # Auto Scaling 最大 Node 數量
+      desired_size = 2                       # Auto Scaling 預期 Node 數量
     }
 
     two = {
-      name = "node-group-2"
+      name = "node-group-2"                 # Node Group name
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.small"]         # EC2 instance types
 
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size     = 1                       # Auto Scaling 最小 Node 數量
+      max_size     = 2                       # Auto Scaling 最大 Node 數量
+      desired_size = 1                       # Auto Scaling 預期 Node 數量
     }
   }
 }
